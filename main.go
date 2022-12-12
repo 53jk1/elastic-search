851995554e3d2b8d20c48384667b0261a1feee3c
Kacper Bąk <kacperbak290@gmail.com>
2022-12-12 22:20:06 +0100

    initial commit

--- main.go
+++ main.go
@@ -1,63 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/elastic/go-elasticsearch/v7"
-	"strings"
+	"github.com/53jk1/elastic-search/app"
 )
 
 func main() {
-	// Create an Elasticsearch client
-	cfg := elasticsearch.Config{
-		Addresses: []string{"http://localhost:9200"},
+	x := app.NewApp()
+	if err := x.Run(); err != nil {
+		panic(err)
 	}
-	es, err := elasticsearch.NewClient(cfg)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Create an index
-	ctx := context.Background()
-	res, err := es.Indices.Create(
-		"myindex",
-		es.Indices.Create.WithBody(strings.NewReader(`{
-			"settings": {
-				"number_of_shards": 1,
-				"number_of_replicas": 0
-			}
-		}`)),
-		es.Indices.Create.WithContext(ctx),
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Index a document
-	res, err = es.Index(
-		"myindex",
-		strings.NewReader(`{"title" : "Test"}`),
-		es.Index.WithDocumentID("1"),
-		es.Index.WithRefresh("true"),
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// Get a document
-	res, err = es.Get(
-		"myindex",
-		"1",
-		es.Get.WithPretty(),
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Delete an index
-	res, err = es.Indices.Delete([]string{"myindex"})
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(res)
 }
